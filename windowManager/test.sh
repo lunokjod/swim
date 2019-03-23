3f69e27b0cd6b4439f876f4af9acaa78d896ecdd
lunokjod <797792+lunokjod@users.noreply.github.com>
2019-03-23 22:30:51 +0100

    test it before run!

---
+++ windowManager/test.sh
@@ -0,0 +1,8 @@
+XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
+xinit ./xinitrc -- \
+    "$XEPHYR" \
+        :100 \
+        -ac \
+        -screen 800x600 \
+        -no-host-grab
+#        -host-cursor
