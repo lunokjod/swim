6481b3d7aa5c77aceb0418d4a85fbcb3be35b428
lunokjod <797792+lunokjod@users.noreply.github.com>
2019-03-23 19:09:25 +0100

    i love the brackets

--- windowManager/config.h
+++ windowManager/config.h
@@ -91,6 +91,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
