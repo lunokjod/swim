962a82b6022b54dc45cdf519b84a1a6e112a389b
lunokjod <797792+lunokjod@users.noreply.github.com>
2020-03-13 17:47:55 +0100

    multiple fonts

--- windowManager/config.h
+++ windowManager/config.h
@@ -14,7 +14,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "monospace:size=10" };
 // { "DejaVu Serif:style=Bold:size=10" }; 
-static const char *fonts[]          = { "Ubuntu:size=11" }; 
+// $ fc-list
+// "JetBrains Mono:style=Regular:size=11", 
+static const char *fonts[]          = { "Ubuntu:size=11",
+										"DejaVu Serif:style=Bold:size=10",
+										"monospace:size=10",
+										"JetBrains Mono:style=Regular:size=11"
+									}; 
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
