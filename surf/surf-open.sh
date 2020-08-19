01bebe964054bccaa9ea86b50c8a75627852b8e8
lunokjod <797792+lunokjod@users.noreply.github.com>
2020-08-19 17:47:22 +0200

    surf-open with uri enabled

--- surf/surf-open.sh
+++ surf/surf-open.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # See the LICENSE file for copyright and license details. 
 #
@@ -9,10 +9,14 @@ uri=""
 if [ "$#" -gt 0 ];
 then
 	uri="$1"
+else
+    uri="https://duckduckgo.com/?kae=t&kaq=-1&kp=-2&kz=-1&kav=1&k1=-1&kaj=m&kak=-1&kax=-1&kap=-1&kau=-1&kg=p"
 fi
 
 runtabbed() {
-	tabbed -dn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" \
+	echo "Opening: " uri
+	tabbed -dn tabbed-surf -r 2 surf -e '' "${uri}" >"$xidfile" \
+#	tabbed -dn tabbed-surf -r 2 surf -e '' >"$xidfile" \
 		2>/dev/null &
 }
 
@@ -26,7 +30,8 @@ else
 	then
 		runtabbed
 	else
-		surf -e "$xid" "$uri" >/dev/null 2>&1 &
+#		surf -e "$xid" >/dev/null 2>&1 &
+		surf -e "$xid" "${uri}" >/dev/null 2>&1 &
 	fi
 fi
 
